ffd1873fd99d05067a02ffd45bd32f9de91f12b3
VadymLysenko <vesioluy@hotmail.com>
2021-05-25 19:19:18 -0500

    shared on github

--- src/test/java/BranchingPractice.java
+++ src/test/java/BranchingPractice.java
@@ -2,6 +2,10 @@ public class BranchingPractice {
 
     public static void main(String[] args) {
 
-
+        //created new project
+        //enable version control
+        //added .gitignore with .idea file and target folder and .iml file
+        //made initial commit by keeping tarck of all files
+        // shared on github
     }
 }
